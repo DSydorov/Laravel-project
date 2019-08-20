897e52941136c76c06d66bd5a482291ca93a12e2
DSydorov <davilwalk.96@gmail.com>
2019-08-20 13:14:29 +0300

    add new files and mod old files

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@
 use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use App\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,11 +18,16 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $roleId = Role::where('name', '=', 'Customer')->first()->id;
     return [
         'name' => $faker->name,
+        'surname' => $faker->LastName,
+        'role_id' => $roleId ? $roleId : 1,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'birthday' =>$faker->dateTimeBetween('-70 years', '-18 years')->format('Y-m-d'),
+        'password' => $faker->password(8), // password
+        'phone' => $faker->phoneNumber,
         'remember_token' => Str::random(10),
     ];
 });
